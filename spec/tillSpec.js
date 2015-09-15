1e89ac5cc04e20dde5066e9320af0b97825fe536
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-09-15 14:07:10 +0100

    Till can record customer names, orders of multiple items and total price

--- spec/tillSpec.js
+++ spec/tillSpec.js
@@ -6,9 +6,29 @@ describe('Till', function() {
   });
 
   describe('taking orders', function() {
-    it('can take an order and recall the item and price', function() {
-      till.takeOrder('Cafe Latte');
-      expect(till.orders).toEqual([['Cafe Latte', 4.75]]);
+    it('can take an order and keep track of the item and price', function() {
+      till.takeOrder(1, 'Cafe Latte');
+      expect(till.orders).toEqual([['Cafe Latte', 1, 'x ', 4.75]]);
+    });
+
+    it('can take multiple orders and keep track of them', function() {
+      till.takeOrder(2, 'Cafe Latte');
+      expect(till.orders).toEqual([['Cafe Latte', 2, "x ", 4.75]]);
+    });
+  });
+
+  describe('getting a total price', function() {
+    it('can calculate the total price for the orders', function() {
+      till.takeOrder(1, 'Cafe Latte');
+      till.takeOrder(1, 'Tea');
+      expect(till.total).toEqual(8.40);
+    });
+  });
+
+  describe('showing customer names', function() {
+    it('can recall the names of the customers', function() {
+      till.takeName('Kirsten');
+      expect(till.customerNames).toEqual(['Kirsten']);
     });
   });
 });
--- src/till.js
+++ src/till.js
@@ -1,8 +1,15 @@
 function Till() {
-  this.cafeData = cafeData;
+  this.menu = cafeData;
   this.orders = [];
+  this.total = 0;
+  this.customerNames = [];
 }
 
-Till.prototype.takeOrder = function(item) {
-  this.orders.push([item, this.cafeData[0].prices[0][item]]);
+Till.prototype.takeOrder = function(multiple, item) {
+  this.orders.push([item, multiple, "x ", this.menu[0].prices[0][item]]);
+  this.total += multiple * (this.menu[0].prices[0][item]);
+};
+
+Till.prototype.takeName = function(name) {
+  this.customerNames.push(name);
 };
