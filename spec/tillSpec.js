6daf9b8940c0cc3619c27ff26f6f956f5010af5e
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-09-16 09:47:14 +0100

    Refactors functions to interact directly with receipt object

--- spec/tillSpec.js
+++ spec/tillSpec.js
@@ -8,25 +8,25 @@ describe('Till', function() {
   describe('taking orders', function() {
     it('can take an order and keep track of the item and price', function() {
       till.takeOrder(1, 'Cafe Latte');
-      expect(till.orders).toEqual([['Cafe Latte', 1, 'x ', 4.75]]);
+      expect(till.receipt.orders).toEqual([['Cafe Latte', 1, 'x ', 4.75]]);
     });
 
     it('can take multiple orders and keep track of them', function() {
       till.takeOrder(2, 'Cafe Latte');
-      expect(till.orders).toEqual([['Cafe Latte', 2, "x ", 4.75]]);
+      expect(till.receipt.orders).toEqual([['Cafe Latte', 2, "x ", 4.75]]);
     });
   });
 
   describe('showing customer names', function() {
     it('can recall a customer name', function() {
       till.takeName('Kirsten');
-      expect(till.customerNames).toEqual(['Kirsten']);
+      expect(till.receipt.names).toEqual(['Kirsten']);
     });
 
     it('can recall multiple customer names', function() {
       till.takeName('Kirsten');
       till.takeName('Gerard');
-      expect(till.customerNames).toEqual(['Kirsten', 'Gerard']);
+      expect(till.receipt.names).toEqual(['Kirsten', 'Gerard']);
     });
   });
 
@@ -35,7 +35,7 @@ describe('Till', function() {
       till.takeOrder(1, 'Cafe Latte');
       till.takeOrder(1, 'Tea');
       till.calculateTax();
-      expect(till.taxAmount).toEqual(0.73);
+      expect(till.receipt.tax).toEqual(0.73);
     });
   });
 
@@ -43,7 +43,7 @@ describe('Till', function() {
     it('can calculate the total price for the orders before adding tax', function() {
       till.takeOrder(1, 'Cafe Latte');
       till.takeOrder(1, 'Tea');
-      expect(till.preTaxTotal).toEqual(8.40);
+      expect(till.receipt.subtotal).toEqual(8.40);
     });
   });
 
@@ -53,7 +53,7 @@ describe('Till', function() {
       till.takeOrder(1, 'Tea');
       till.calculateTax();
       till.calculateAfterTaxTotal();
-      expect(till.afterTaxTotal).toEqual(9.13);
+      expect(till.receipt.total).toEqual(9.13);
     });
   });
 
@@ -66,7 +66,7 @@ describe('Till', function() {
       till.takeOrder(1, 'Cortado');
       till.calculateTax();
       till.calculateAfterTaxTotal();
-      expect(till.produceReceipt()).toEqual(
+      expect(till.printReceipt()).toEqual(
         {'Names': ['Kirsten', 'Gerard'],
         'Orders': [[ 'Cafe Latte', 1, 'x ', 4.75 ], [ 'Tiramisu', 1, 'x ', 11.40 ], [ 'Cortado', 1, 'x ', 4.55 ]], 'Tax': 1.79, 'Total': 22.49 });
     });
--- src/till.js
+++ src/till.js
@@ -1,36 +1,36 @@
 function Till() {
   this.menu = cafeData;
-  this.orders = [];
-  this.preTaxTotal = 0;
-  this.customerNames = [];
+  // this.orders = [];
+  // this.preTaxTotal = 0;
+  // this.customerNames = [];
   this.taxRate = 8.64/100;
-  this.taxAmount = 0;
-  this.afterTaxTotal = 0;
-  this.receipt = {'Names': [], 'Orders': [], 'Tax': 0, 'Subtotal', 0, 'Total': 0}
+  // this.taxAmount = 0;
+  // this.afterTaxTotal = 0;
+  this.receipt = {'names': [], 'orders': [], 'tax': 0, 'subtotal': 0, 'total': 0}
 }
 
 Till.prototype.takeOrder = function(multiple, item) {
   // this.orders.push([item, multiple, "x ", this.menu[0].prices[0][item]]);
   // this.preTaxTotal += multiple * (this.menu[0].prices[0][item]);
-  this.receipt.Orders.push([item, multiple, "x ", this.menu[0].prices[0][item]]);
-  this.receipt.Subtotal += multiple * (this.menu[0].prices[0][item]);
+  this.receipt.orders.push([item, multiple, "x ", this.menu[0].prices[0][item]]);
+  this.receipt.subtotal += multiple * (this.menu[0].prices[0][item]);
 };
 
 Till.prototype.takeName = function(name) {
   // this.customerNames.push(name);
-  this.receipt.Names.push(name);
+  this.receipt.names.push(name);
 };
 
 Till.prototype.calculateTax = function() {
   // this.taxAmount += +(this.preTaxTotal * this.taxRate).toFixed(2);
-  this.receipt.Tax = +(this.receipt.Subtotal * this.taxRate).toFixed(2);
+  this.receipt.tax = +(this.receipt.subtotal * this.taxRate).toFixed(2);
 };
 
 Till.prototype.calculateAfterTaxTotal = function() {
   // this.afterTaxTotal += +(this.preTaxTotal + this.taxAmount);
-  this.receipt.Total = +(this.receipt.Subtotal + this.receipt.Tax);
+  this.receipt.total = +(this.receipt.subtotal + this.receipt.tax);
 };
 
-// Till.prototype.produceReceipt = function() {
-//   return {'Names': this.customerNames, 'Orders': this.orders, 'Tax': (this.taxAmount)/1, 'Total': (this.afterTaxTotal)/1};
-// };
+Till.prototype.printReceipt = function() {
+  return {'Names': this.receipt.names, 'Orders': this.receipt.orders, 'Tax': this.receipt.tax, 'Total': this.receipt.total};
+};
